a4a223f3dfa19639d7e18028f2cb2b3adac8db5f
Joey Peterson <joey.peterson15@gmail.com>
2022-01-30 10:24:11 -0800

    grid

--- my-app/src/PathfindingVisualizer/PathfindingVisualizer.js
+++ my-app/src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -1,14 +1,38 @@
-
+import { useState, useEffect } from 'react'
+import Node from './Node/Node.js'
 
 import './PathfindingVisualizer.css'
 
 
 function PathfindingVisualizer () {
 
+    const [nodes, setNodes] = useState([])
+
+    useEffect(() => {
+        const setup = []
+        for (let row = 0; row < 20; row++) {
+            const currentRow = []
+            for (let col = 0; col < 50; col++) {
+                const currentNode = {
+                    col,
+                    row,
+                }
+                currentRow.push(currentNode)
+            }
+            setup.push(currentRow)
+        }
+        setNodes(setup)
+    })
+
     return (
-        null
+        <div className="grid">
+            {nodes.map((row) => (
+                <div>
+                    {row.map((node, nodeIndex) => <Node />)}
+                </div>
+            ))}
+        </div>
     )
 }
 
 export default PathfindingVisualizer
- 
